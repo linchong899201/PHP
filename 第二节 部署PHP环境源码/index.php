a41c1d89800eb3cad6a2507c126f1779fd0e6811
Peter <linchong899201@hotmail.com>
2017-07-22 22:32:37 +0800

    零基础学PHP课堂笔记和源码第一节到第四节
    请下载看零基础学PHP课堂笔记和源码第一节到第四节

---
+++ 第二节 部署PHP环境源码/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+	<title>我的网站</title>
+</head>
+
+<body>
+	<h1>我的网站</h1>
+	<p><?php echo 'hello world!'; ?></p>
+
+	<p><?php //print 'hello world!'; ?></p>
+</body>
+
+</html>
---
+++ 第四节 显示报错信息源码/index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+	<title>我的网站</title>
+</head>
+
+<body>
+	<h1>我的网站</h1>
+	<p><?php echo 'hello world!'; ?></p>
+
+	<p><?php //print 'hello world!'; ?></p>
+
+	<?php 
+		//$name = "lin";
+		$first_name = "lin"
+	?>
+	<p><?php 
+		//echo '你好，' . $name;
+	?></p>
+	<p><?php //echo '你好1，' . $name;
+	?></p>
+
+	<?php 
+		$name = "lin";
+		//$name = $_GET ['name'] ;
+		//echo "<p>你好 $name ，$name 吃饭了吗? </p>;
+		echo "你好 $name ，$name 吃饭了吗?";
+	?>
+	<p><?php echo $first_name;?></p>
+</body>
+
+</html>
